3f1e4bd32fa8967356dcd2f5036a937f30a3d238
Josh Geronimo <joshuageronimo112398@gmail.com>
2023-01-08 06:57:51 -0800

    Create the Character Schema

--- inventory-application/models/character.js
+++ inventory-application/models/character.js
@@ -5,5 +5,11 @@ const Schema = mongoose.Schema;
 const CharacterSchema = new Schema({
   name: String,
   title: String,
-  vision:
+  vision: { type: Schema.Types.ObjectId, ref: "Vision" },
+  weaponType: { type: Schema.Types.ObjectId, ref: "Weapon" },
+  role: [{ type: Schema.Types.ObjectId, ref: "Role" }],
+  rating: Number,
+  amount: Number,
 });
+
+module.exports = mongoose.model("Character", CharacterSchema);
