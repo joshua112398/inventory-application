3ec9ccf85f2084480d1cef818412f22a12384e55
Josh Geronimo <joshuageronimo112398@gmail.com>
2023-03-01 11:37:35 -0800

    Allows creation of new weapon
    Submitting weapon creation form will now add new weapon to
    database afrer validation

--- inventory-application/controllers/weaponController.js
+++ inventory-application/controllers/weaponController.js
@@ -37,6 +37,46 @@ exports.weaponDetail = async (req, res, next) => {
 exports.weaponCreateGet = async (req, res, next) => {
   res.render("weaponCreate", {
     title: "Add New Weapon",
+    weapon: {name: '', description: ''},
     errors: null,
   });
 };
+
+exports.weaponCreatePost = [
+  body("name")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("Name must be specified")
+    .isAlphanumeric()
+    .withMessage("Name must contain only numbers and letters"),
+  body("description")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("Description must be specified"),
+  
+  // Process form
+  (req, res, next) => {
+    const errors = validationResult(req);
+    // If errors exist, then rerender form with inputted values
+    if (!errors.isEmpty()) {
+      res.render("weaponCreate", {
+        title: "Add New Weapon",
+        weapon: req.body,
+        errors: errors.array(),
+      });
+    }
+    // If no errors, then add weapon to database and render weapon detail page
+    const weapon = new Weapon({
+      name: req.body.name,
+      description: req.body.description,
+    });
+    weapon.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      res.redirect(weapon.url);
+    });
+  },
+];
