2b0cb737a2d28cc1ecee99ca249f1f3376a39e4f
Josh Geronimo <joshuageronimo112398@gmail.com>
2023-03-13 03:30:40 -0700

    Changes role selection to checkboxes
    Allows multiple roles to be assigned to the new character.
    If an error is detected, the rerendered form will retain the status
    of each checkbox, e.g. it will remain checked if it was checked
    prior to the previous submission.

--- inventory-application/controllers/characterController.js
+++ inventory-application/controllers/characterController.js
@@ -71,7 +71,7 @@ exports.characterCreateGet = async (req, res, next) => {
     // Render form
     res.render("characterCreate", {
       title: "Add a Character",
-      character: {name: "", title: "", vision: {}, weapon: {}, role: {}, rating:0, amount:0},
+      character: {name: "", title: "", vision: {}, weapon: {}, role: ['', ''], rating:0, amount:0},
       visions: visions,
       weapons: weapons,
       roles: roles,
@@ -97,34 +97,90 @@ exports.characterCreatePost = [
     .isLength({min: 1})
     .escape()
     .withMessage("Title must be specified")
-    .isAlphanumeric()
-    .withMessage("Name must contain only letters and numbers."),
+    .isAlphanumeric("en-US", {ignore: " -'"})
+    .withMessage("Title must contain only letters and numbers."),
+  body("vision")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("Vision must be specified"),
+  body("role")
+    .exists()
+    .withMessage("At least one role must be specified"),
+  body("weapon")
+    .trim()
+    .escape(),
+  body("rating")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("Rating must be specified")
+    .isNumeric()
+    .withMessage("Rating must be a number"),
+  body("amount")
+    .trim()
+    .isLength({min: 1})
+    .escape()
+    .withMessage("Amount must be specified")
+    .isNumeric()
+    .withMessage("Amount must be a number"),
 
   // Process fields
-  (req, res, next) => {
-    const errors = validationResult(req);
-    
-    // Rerender form if there are errors during validation/sanitization
-    if (!errors.isEmpty()) {
-      res.render("characterCreate", {
-        title: "Add a Character",
-        character: req.body,
-        errors: errors.array(),
-      });
-      return;
-    }
+  async (req, res, next) => {
+    try {
+      const errors = validationResult(req);
+      console.log(req.body);
+      
+      // Rerender form if there are errors during validation/sanitization
+      if (!errors.isEmpty()) {
+        // Fetch all available visions/weapons/roles to render them as options in the form
+        const visions = await Vision.find({})
+          .sort({name: 1})
+          .exec();
+        const weapons = await Weapon.find({})
+          .sort({name: 1})
+          .exec();
+        const roles = await Role.find({})
+          .sort({name: 1})
+          .exec();
 
-    // Add role to database and render role detail if no errors
-    const character = new Character({
-      name: req.body.name,
-      title: req.body.title,
-      vision: req.body.vision._id,
-    });
-    character.save((err) => {
-      if (err) {
-        return next(err);
+        // Turn the roles field into an array if it's not already one (e.g. when only one role was checked
+        req.body.role = [].concat(req.body.role);
+        console.log(req.body);
+
+        // Rerender form using previously entered data
+        res.render("characterCreate", {
+          title: "Add a Character",
+          character: req.body,
+          visions: visions,
+          weapons: weapons,
+          roles: roles,
+          errors: errors.array(),
+        });
+        return;
       }
-      res.redirect(role.url);
-    });
+
+
+      // Add role to database and render role detail if no errors
+      const character = new Character({
+        name: req.body.name,
+        title: req.body.title,
+        vision: req.body.vision,
+        // If no weapon equipped, set value to null
+        weapon: (req.body.weapon === '') ? null : req.body.weapon,
+        role: req.body.role,
+        rating: req.body.rating,
+        amount: req.body.amount,
+      });
+      character.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect(character.url);
+      });
+    }
+    catch(err) {
+      return next(err);
+    }
   },
 ];
