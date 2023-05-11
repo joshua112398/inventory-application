717f57da6a6e4575a92fe5654b85899cccec3a4f
Josh Geronimo <joshuageronimo112398@gmail.com>
2023-05-10 19:00:56 -0700

    Fix image content type bug

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -22,6 +22,7 @@ const s3 = new S3Client({
 const s3Storage = multerS3({
   s3: s3,
   bucket: process.env.AWS_BUCKET_NAME,
+  contentType: multerS3.AUTO_CONTENT_TYPE,
   metadata: (req, file, cb) => {
     cb(null, {fieldname: file.fieldname})
   },
